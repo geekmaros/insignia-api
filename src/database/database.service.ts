a9b8ed8ed46751e4ae0c6e902bbcf4bb5d56c5e8
Abdulrasaq Mustapha <razicruz@gmail.com>
2025-12-15 22:45:20 +0100

    fix: fix app after removing prisma

---
+++ src/database/database.service.ts
@@ -0,0 +1,5 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseService {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DatabaseService } from '../database/database.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly databaseService: DatabaseService) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
     return `This action returns all users`;
+    // return this.databaseService.user.findMany();
   }
 
   findOne(id: number) {
