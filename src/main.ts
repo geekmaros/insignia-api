44aad90e00c31925c3c02b854a9ad66e1cf94b38
Abdulrasaq Mustapha <razicruz@gmail.com>
2026-01-27 16:06:14 +0100

    feat: update port

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  const port = parseInt(process.env.PORT ?? '8080', 10);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 API running on http://localhost:${port}`);
 }
